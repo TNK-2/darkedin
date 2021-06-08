1808ec319244ad7bbdbfe5fc118b3059b9d909e2
Yu-s <yuuu1993g@gmail.com>
2021-06-08 23:07:11 +0900

    use bookmark model

--- src/app/models/bookmark.ts
+++ src/app/models/bookmark.ts
@@ -4,17 +4,18 @@ export interface Bookmark {
   id: number;
   title: string;
   url: string;
-  registDate: Date;
+  summary: string;
+  registDate: string;
   isFavorite: boolean;
   tags: Tag[];
   comment: string;
 }
 
 export const MOCK_BOOKMARKS: Bookmark[] = [
-  {id: 1, title: "test1", url: "https://logmi.jp/tech/articles/324298", registDate: new Date(), isFavorite: true, tags: [{id: 1, name: "taro"}], comment: "test"},
-  {id: 2, title: "test1", url: "https://logmi.jp/tech/articles/324298", registDate: new Date(), isFavorite: true, tags: [{id: 1, name: "taro"}], comment: "test"},
-  {id: 3, title: "test1", url: "https://logmi.jp/tech/articles/324298", registDate: new Date(), isFavorite: true, tags: [{id: 1, name: "taro"}], comment: "test"},
-  {id: 4, title: "test1", url: "https://logmi.jp/tech/articles/324298", registDate: new Date(), isFavorite: true, tags: [{id: 1, name: "taro"}], comment: "test"},
-  {id: 5, title: "test1", url: "https://logmi.jp/tech/articles/324298", registDate: new Date(), isFavorite: true, tags: [{id: 1, name: "taro"}], comment: "test"},
-  {id: 6, title: "test1", url: "https://logmi.jp/tech/articles/324298", registDate: new Date(), isFavorite: true, tags: [{id: 1, name: "taro"}], comment: "test"}
+  {id: 1, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest", registDate: "2021/06/08", isFavorite: true, tags: [{id: 1, name: "taro"}], comment: "test"},
+  {id: 2, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttest", registDate: "2021/06/08", isFavorite: true, tags: [{id: 1, name: "taro"}, {id: 2, name: "taro2"}], comment: "test"},
+  {id: 3, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttest", registDate: "2021/06/08", isFavorite: true, tags: [{id: 1, name: "taro"}], comment: "test"},
+  {id: 4, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttest", registDate: "2021/06/08", isFavorite: true, tags: [{id: 1, name: "taro"}], comment: "test"},
+  {id: 5, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttest", registDate: "2021/06/08", isFavorite: true, tags: [{id: 1, name: "taro"}], comment: "test"},
+  {id: 6, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttest", registDate: "2021/06/08", isFavorite: true, tags: [{id: 1, name: "taro"}], comment: "test"}
 ]
