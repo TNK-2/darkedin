b3a867282fb6907245df1d8df28a331d0c3a0f40
Yu-s <yuuu1993g@gmail.com>
2021-06-08 23:39:46 +0900

    create mock: tag user

--- src/app/models/tag.ts
+++ src/app/models/tag.ts
@@ -2,3 +2,9 @@ export interface Tag {
   id: number;
   name: string;
 }
+
+export const MOCK_TAGS: Tag[] = [
+  {id: 1, name: "あとで読む"},
+  {id: 2, name: "技術"},
+  {id: 3, name: "飯"}
+]
---
+++ src/app/models/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+  id: string;
+  name: string;
+  imageUrl: string;
+}
+
+export const MOCK_USER: User = {id: "yuuu1993g", name: "tanuki", imageUrl: "https://cdn.profile-image.st-hatena.com/users/yuuu1993g/profile.png"}
