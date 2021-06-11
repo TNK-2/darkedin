a370d1c85ce2b624de23d0df0e2c331f23f689ac
TNK-2 <yuuu1993g@gmail.com>
2021-06-11 22:33:46 +0900

    fix layout

--- darkedin-front/src/app/models/bookmark.ts
+++ darkedin-front/src/app/models/bookmark.ts
@@ -4,20 +4,19 @@ export interface Bookmark {
   id: number;
   title: string;
   url: string;
-  summary: string;
   registDate: string;
   isFavorite: boolean;
   tags: Tag[];
   comment: string;
 }
 
-export const MOCK_BOOKMARK: Bookmark = {id: 1, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttesttesttesttesttesttesttesttesttestte", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"}
+export const MOCK_BOOKMARK: Bookmark = {id: 1, title: "プログラマによるプログラミングのためのBGMなど、仕事や勉強の邪魔にならない無料で使えそうなBGM集。2021年版 － Publickey", url: "https://logmi.jp/tech/articles/324298", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"}
 
 export var MOCK_BOOKMARKS: Bookmark[] = [
-  {id: 1, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"},
-  {id: 2, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttest", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"},
-  {id: 3, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttest", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"},
-  {id: 4, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttest", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"},
-  {id: 5, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttest", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"},
-  {id: 6, title: "test1", url: "https://logmi.jp/tech/articles/324298", summary: "testtesttest", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"}
+  {id: 1, title: "プログラマによるプログラミングのためのBGMなど、仕事や勉強の邪魔にならない無料で使えそうなBGM集。2021年版 － Publickey", url: "https://logmi.jp/tech/articles/324298", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"},
+  {id: 2, title: "test1", url: "https://logmi.jp/tech/articles/324298", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"},
+  {id: 3, title: "test1", url: "https://logmi.jp/tech/articles/324298", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"},
+  {id: 4, title: "test1", url: "https://logmi.jp/tech/articles/324298", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"},
+  {id: 5, title: "test1", url: "https://logmi.jp/tech/articles/324298", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"},
+  {id: 6, title: "test1", url: "https://logmi.jp/tech/articles/324298", registDate: "2021/06/08", isFavorite: true, tags: [MOCK_TAGS[0], MOCK_TAGS[1]], comment: "test"}
 ]
