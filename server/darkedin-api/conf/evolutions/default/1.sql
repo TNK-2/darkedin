07a584358385d1d5272ba8972258ffbd1532e864
TNK-2 <yuuu1993g@gmail.com>
2021-06-20 10:23:18 +0900

    create migration user

---
+++ server/darkedin-api/conf/evolutions/default/1.sql
@@ -0,0 +1,15 @@
+# ---!Ups
+create table "user" {
+	"id"                  int auto_increment primary key,
+    "name"                varchar(255) not null,
+    "git_name"            varchar(255) not null,
+    "git_url"             varchar(255) not null,
+    "avatar_url"          varchar(255) not null,
+    "access_token"        varchar(64)  null unique,
+    "token_expire_at"     datetime(6)  null
+    "created_at"          datetime(6)  not null,
+    "updated_at"          datetime(6)  not null
+}
+
+# --- !Downs
+drop table "user"
