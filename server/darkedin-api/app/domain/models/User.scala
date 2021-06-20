d7c19d097b7a1ec63948207c7df2612eac6275e5
TNK-2 <yuuu1993g@gmail.com>
2021-06-20 10:22:59 +0900

    fix user domain

--- server/darkedin-api/app/domain/models/User.scala
+++ server/darkedin-api/app/domain/models/User.scala
@@ -1,3 +1,15 @@
 package domain.models
 
-case class User(id:String, name: String)
+import akka.http.scaladsl.model.DateTime
+
+case class User(
+  val id:Int,
+  val name: String,
+  val gitUrl: String,
+  val avatarUrl: String,
+  val accessToken: String,
+  val tokenExpiredAt: DateTime,
+  val createdAt: DateTime,
+  val updatedAt: DateTime
+)
+
--- server/darkedin-api/app/domain/repositories/UserRepository.scala
+++ server/darkedin-api/app/domain/repositories/UserRepository.scala
@@ -2,7 +2,9 @@ package domain.repositories
 
 import domain.models.User
 
+import scala.concurrent.Future
+
 trait UserRepository {
-  def findById(id: String)
-  def save(user: User)
+  def findById(id: String): Future[Option[String]]
+  def save(user: User): Future[Unit]
 }
